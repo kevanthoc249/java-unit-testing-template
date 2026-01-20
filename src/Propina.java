a0964c177d8d4f7ba54fa466785394bc16e7b6bb
kevanthoc249 <kachalan@espe.edu.ec>
2026-01-20 08:57:31 -0500

    Kevin Chalan

--- src/Propina.java
+++ src/Propina.java
@@ -8,8 +8,16 @@ public class Propina {
    *  - total = subtotal + subtotal*(porcentaje/100)
    *  - si una regla no se cumple: lanzar IllegalArgumentException
    */
-  public static double totalConPropina(double subtotal, double porcentajePropina) {
-    // TODO: reemplazar esta implementacion
-    throw new UnsupportedOperationException("Pendiente de implementar");
+public static double totalConPropina(double subtotal, double porcentajePropina) {
+
+    if (subtotal < 0) {
+      throw new IllegalArgumentException("El subtotal no puede ser negativo");
+    }
+
+    if (porcentajePropina < 0 || porcentajePropina > 100) {
+      throw new IllegalArgumentException("El porcentaje debe estar entre 0 y 100");
+    }
+
+    return subtotal + subtotal * (porcentajePropina / 100.0);
   }
 }
